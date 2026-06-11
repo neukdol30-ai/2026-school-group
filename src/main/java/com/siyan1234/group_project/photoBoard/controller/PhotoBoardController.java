6a03801e3e4eff9597e9ff44959bbd17008b8776
tjoeunis205 <knw9526@gmail.com>
2026-06-11 16:24:56 +0900

    0.0.1 photoBoard

--- src/main/java/com/siyan1234/group_project/photoBoard/controller/PhotoBoardController.java
+++ src/main/java/com/siyan1234/group_project/photoBoard/controller/PhotoBoardController.java
@@ -5,8 +5,7 @@ import com.siyan1234.group_project.photoBoard.service.PhotoBoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,31 +14,45 @@ import java.util.List;
 @RequestMapping("/photoBoard")
 public class PhotoBoardController {
 
-
     private final PhotoBoardService photoBoardService;
 
-    // 사진 리뷰 게시판 목록
     @GetMapping("/list")
     public String list(Model model) {
-
-        List<PhotoBoardDto> boardList =
-                photoBoardService.getList();
-
+        List<PhotoBoardDto> boardList = photoBoardService.getList();
         model.addAttribute("boardList", boardList);
-
         return "photoBoard/list";
     }
 
-    // 사진 리뷰 게시글 작성 페이지
     @GetMapping("/write")
     public String write() {
         return "photoBoard/write";
     }
 
-    // 사진 리뷰 게시글 상세보기
+    @PostMapping("/write")
+    public String writeProcess(PhotoBoardDto photoBoardDto) {
+
+        System.out.println("title = " + photoBoardDto.getTitle());
+        System.out.println("content = " + photoBoardDto.getContent());
+        System.out.println("rating = " + photoBoardDto.getRating());
+
+        photoBoardDto.setMemberNo(1);
+        photoBoardDto.setRestaurantNo(1);
+
+        photoBoardService.write(photoBoardDto);
+
+        return "redirect:/photoBoard/list";
+    }
+
     @GetMapping("/view")
-    public String view() {
+    public String view(@RequestParam int no, Model model) {
+        PhotoBoardDto board = photoBoardService.getBoard(no);
+        model.addAttribute("board", board);
         return "photoBoard/view";
     }
 
+    @GetMapping("/delete")
+    public String delete(@RequestParam int no) {
+        photoBoardService.delete(no);
+        return "redirect:/photoBoard/list";
+    }
 }
