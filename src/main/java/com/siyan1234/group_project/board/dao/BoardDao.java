8252b89e9a2aa60ea050ee4fe1789db5736a6f1f
tjoeunis205 <fkrdbs1278@gmail.com>
2026-06-15 11:43:10 +0900

    0.2.1 1:1문의 기능 개선

---
+++ src/main/java/com/siyan1234/group_project/board/dao/BoardDao.java
@@ -0,0 +1,26 @@
+package com.siyan1234.group_project.board.dao;
+
+import com.siyan1234.group_project.board.dto.BoardDto;
+import com.siyan1234.group_project.board.dto.PageDto;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface BoardDao {
+    List<BoardDto> listBoard(PageDto pageDto);
+
+    int countBoard(PageDto pageDto);
+
+    BoardDto viewBoard(int no);
+    void updateHit(int no);
+
+    // 글 저장
+    void writeBoard(BoardDto boardDto);
+
+    // 글 수정
+    void editBoard(BoardDto boardDto);
+    // 글 삭제
+    void deleteBoard(int no);
+}
+
